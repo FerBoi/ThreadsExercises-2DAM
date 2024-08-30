b8f99e56335cffe4786f5fb7c7a98436a294f22f
Fernando GJ <fernandogajib@gmail.com>
2024-08-30 14:15:04 +0200

    clase init - ejer 1 forma 2 hilos

---
+++ src/ejercicio_uno/segunda_forma/Init.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package ejercicio_uno.segunda_forma;
+
+/**
+ * -- SEMAFOROS --
+ * 
+ * En una pajarería están teniendo problemas para tener a todos sus loros felices. Los loros
+ * comparten una jaula en la que hay un plato con comida y un columpio para hacer ejercicio. Todos
+ * los loros repiten la misma rutina una y otra vez: primero comen del plato y después se columpian.
+ * Pero se encuentran con el inconveniente de que sólo tres de ellos pueden comer del plato al
+ * mismo tiempo y que en el columpio solo cabe un pájaro.
+ * 
+ * @author Fernando GJ
+ */
+public class Init {
+
+    public static void main(String[] args) {
+        new Jaula("50").empezarSimulacion();
+
+    } // end main
+
+} // end Init
