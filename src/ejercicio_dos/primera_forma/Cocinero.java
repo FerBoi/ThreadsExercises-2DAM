3e94cd2a46f343489af3284d22b5872c484e4d9a
Fernando GJ <fernandogajib@gmail.com>
2024-08-30 14:11:58 +0200

    clase cocinero - ejer 2 forma 1 hilos

---
+++ src/ejercicio_dos/primera_forma/Cocinero.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ejercicio_dos.primera_forma;
+
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Fernando GJ
+ */
+public class Cocinero extends Thread {
+    private final Fabrica MI_TRABAJO;
+    
+    public Cocinero(Fabrica trabajo) {
+        this.MI_TRABAJO = trabajo;
+    }
+
+    @Override
+    public void run() {
+        Random r = new Random();
+        
+        while(true) {
+            String pedido = this.MI_TRABAJO.pillarPedido(this);
+            
+            try {
+                Thread.sleep(r.nextInt(100, 1001));
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Cocinero.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            this.MI_TRABAJO.dejarPedidoBandeja(pedido);
+        }
+    }
+
+} // end Cocinero
