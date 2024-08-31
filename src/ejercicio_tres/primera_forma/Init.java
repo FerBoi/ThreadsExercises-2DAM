9680b08b72b4ceb273dd5c81576f98edb42e5f7a
Fernando GJ <fernandogajib@gmail.com>
2024-08-31 12:36:28 +0200

    clase init - ejer 3 forma 1 hilos

---
+++ src/ejercicio_tres/primera_forma/Init.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package ejercicio_tres.primera_forma;
+
+/**
+ *
+ * @author Fernando GJ
+ */
+public class Init {
+
+    public static void main(String[] args) {
+         new Jaula("50").empezarSimulacion();
+
+    } // end main
+
+} // end Init
