41e40a54b60d1e2d1420cef6ca7758bb300eb06a
Fernando GJ <fernandogajib@gmail.com>
2024-08-31 13:17:56 +0200

    clase empaquetador - ejer 4 forma 1 hilos

---
+++ src/ejercicio_cuatro/primera_forma/Empaquetador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ejercicio_cuatro.primera_forma;
+
+/**
+ *
+ * @author Fernando GJ
+ */
+public class Empaquetador extends Thread {
+
+    @Override
+    public void run() {
+        while(true)
+            PlantaEmbotelladora.empaquetarCaja();
+    }
+
+} // end Empaquetador
