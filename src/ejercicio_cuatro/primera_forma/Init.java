2e6fdcca79b0262a62c53db9372b7a702d654af0
Fernando GJ <fernandogajib@gmail.com>
2024-08-31 13:18:29 +0200

    clase init - ejer 4 forma 1 hilos

---
+++ src/ejercicio_cuatro/primera_forma/Init.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package ejercicio_cuatro.primera_forma;
+
+/**
+ * La embotelladora: puedan estar trabajando al mismo tiempo varios embotelladores. Los
+ * embotelladores pueden colocar al mismo tiempo botellas en la caja.
+ * @author Fernando GJ
+ */
+public class Init {
+
+    public static void main(String[] args) {
+        PlantaEmbotelladora.empezarSimulacion();
+
+    } // end main
+
+} // end Init
